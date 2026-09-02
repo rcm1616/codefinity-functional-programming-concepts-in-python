acd710012e7a03afa58ae005fcff7bc200074b98
rcm1616 <>
2026-09-02 07:15:59 +0000

    Challenge: Unpacking Tuple Elements

---
+++ packing_and_unpacking_in_python/challenge_unpacking_tuple_elements/main.py
@@ -0,0 +1,13 @@
+def sum_first_last(numbers):
+    if len(numbers) == 1:
+        return numbers[0] * 2
+    else:
+        first, *midle, last = numbers
+        return first + last
+
+output1 = sum_first_last((1, 2, 3, 4, 5))
+print(output1)
+output2 = sum_first_last((10, 20))
+print(output2)
+output3 = sum_first_last((7,))
+print(output3)

3afbcf28bede7337ddff8ac1523332a8b0d2c9a6
rcm1616 <>
2026-09-02 08:54:04 +0000

    Challenge: Returning a Multiplier Function

---
+++ variable_scope_nested_functions_and_closures/challenge_returning_a_multiplier_function/main.py
@@ -0,0 +1,12 @@
+def make_multiplier(n):
+    def multiplier(x):
+        return n*x
+    return multiplier
+
+times3 = make_multiplier(3)
+result1 = times3(10)
+print(result1)
+
+times5 = make_multiplier(5)
+result2 = times5(4)
+print(result2)
